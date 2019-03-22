cc68fcf314992f6b94a9b6df9bed19965323684b
Shivam Shakti <shivam.shakti365@gmail.com>
2019-03-22 23:15:20 +0530

    Add model to the agent

--- src/run.py
+++ src/run.py
@@ -1,15 +1,22 @@
 from agent import AntAgent
+from sac import SAC
 
 num_iterations = 2
-num_steps = 100
+num_steps = 1000
 
 # Initialise policy
+config = dict()
+config['epochs'] = 2
+config['learning_rate'] = 0.01
+config['gamma'] = 0.9
+model = SAC(config)
 
 # Initialise agent, environment and policy
 render = True
 agent = AntAgent(render=render, model=None)
 
-# Initialize replay memory 
+# Initialize replay memory
+
 
 # For iteration:
 for iteration in range(num_iterations):
@@ -24,11 +31,12 @@ for iteration in range(num_iterations):
             # If episode is completed add it to replay memory
             if step != 0:
                 agent.buffer.add_episode(episode_id)
+                print ("episode: ", episode_id)
 
             # Initialize episode
             transition['current_observation'] = agent.reset_environment()
             episode_id = agent.buffer.create_episode()
-            done=False
+            done = False
             transition = dict()
 
         transition['current_observation'] = agent.current_observation
@@ -42,6 +50,7 @@ for iteration in range(num_iterations):
         # Add this transition to the replay buffer
         agent.buffer.add_sample(episode_id, transition)
 
-
+    agent.model = model
+    print (agent.buffer.observations)
     # Train the SAC model with transitions in replay buffer.
     agent.learn()
